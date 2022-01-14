c45b0d40d514c35ae2445ac3703da004fc694102
michel-heon <heon.michel@uqam.ca>
2022-01-14 09:32:59 -0500

    Correction bugs de l'api de gestion des propriétés de connexions à VIVO

--- releng/ca.uqam.tool.vivo-proxy.local/00-env.sh
+++ releng/ca.uqam.tool.vivo-proxy.local/00-env.sh
@@ -14,7 +14,7 @@ export PATH=$PATH:~/.local/bin:./
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"  # variable contenant le nom du répertoire de ce script
 
 export VIVO_PROXY_SRC_HOME=$(cd $SCRIPT_DIR/../../bundles/ca.uqam.tool.vivo-proxy ; pwd) # Emplacement du plugin de vivo-proxy
-export VIVO_PROXY_CLIENT_SRC_HOME=$(cd $SCRIPT_DIR/../../bundles/ca.uqam.tool.vivo-proxy ; pwd) # Emplacement du plugin de vivo-proxy
+export VIVO_PROXY_CLIENT_SRC_HOME=$(cd $SCRIPT_DIR/../../bundles/ca.uqam.tool.vivo-proxy.client ; pwd) # Emplacement du plugin de vivo-proxy
 
 export VIVO_PROXY_TARGET=$(cd $VIVO_PROXY_SRC_HOME/target/ca.uqam.tool.vivo-proxy-1.0.1-SNAPSHOT ; pwd) # Emplacement du target à déployer
 export PROFILE=localhost-jetty     #l'un des profiles disponibles dans settings.xml
--- releng/ca.uqam.tool.vivo-proxy.local/01-generate-sources.sh
+++ releng/ca.uqam.tool.vivo-proxy.local/01-generate-sources.sh
@@ -10,9 +10,8 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null ; pwd )"
 source $SCRIPT_DIR/00-env.sh
 cd $VIVO_PROXY_SRC_HOME
-mvn -T2C clean generate-sources package -DskipGen=false -s settings.xml -Plocalhost
-
+mvn -T2C clean generate-sources package -DskipGen=false -s settings.xml --activate-profiles $PROFILE
 # Compilation vivo-proxy-client
-cd $VIVO_PROXY_CLIENT_SRC_HOME
-mvn -T2C clean generate-sources package 
+# cd $VIVO_PROXY_CLIENT_SRC_HOME
+# mvn -T2C clean generate-sources package 
 
